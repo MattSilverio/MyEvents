8e7f2c822f8dc48988d9df0cdd34da81c95b6540
Matheus Phillipo Silverio Silva <matheusphillipo@gmail.com>
2022-07-04 18:17:17 -0300

    Troca de cores e header responsivo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,24 @@
-
+import { useState } from "react"
+import { List, XCircle } from "phosphor-react"
 import { Link} from "react-router-dom"
 
 
 import styles from "./style-header.module.css"
 
 export function Header(){
+    const [showMenu, setShowMenu] = useState(false)
 
+    let menuMobile
+
+    if(showMenu){
+        menuMobile = <div className={styles.menuMobile}>
+        <XCircle size={32} color="white" onClick={() => setShowMenu(false)}/>
+        <nav className={styles.navMobile}>
+            <Link to="/events">Eventos</Link>
+            <Link to="/login">Login</Link>
+        </nav>
+    </div>
+    }
 
     return(
         <header className={styles.header}>
@@ -13,10 +26,14 @@ export function Header(){
                 <Link to = "/">MyEvents</Link>
             </h1>
 
-            <nav>
-                <Link to="/events">Eventos</Link>
-                <Link to="/login">Login</Link>
-            </nav>
+            <div>
+                    <List size={32}  onClick={() => setShowMenu(!showMenu)}/>
+                <nav className={styles.nav}>
+                    <Link to="/events">Eventos</Link>
+                    <Link to="/login">Login</Link>
+                </nav>
+            </div>
+            {menuMobile}
         </header>
     )
 }
